196b8b4c3b4d131f81689ba7a7e937dff3e29b88
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-01-30 19:10:04 +0530

    Update __main__.py

--- MusicAndVideo/__main__.py
+++ MusicAndVideo/__main__.py
@@ -19,5 +19,13 @@ async def main():
     await arq.close()
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="MusicAndVideo"),
+)
+
+bot.start()
+run()
