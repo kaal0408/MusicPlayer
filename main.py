07fe611c629e3c33760a3508abb7842634f272a4
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-01-30 19:17:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from pytgcalls import idle
+
+from config import call_py
+from MusicAndVideo.quote import arq
+
+
+async def main():
+    await call_py.start()
+    print(
+        """
+    ------------------
+   | Userbot Started! |
+    ------------------
+"""
+    )
+    await idle()
+    await arq.close()
+
+
+bot.start()
+run()
